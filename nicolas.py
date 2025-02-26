93b0a3063c63426937d1455bd4f2ae2a11a25aef
Nicolas <nicolascesarmendes@gmail.com>
2025-02-26 16:33:41 -0300

    NicolasFinal

--- nicolas.py
+++ nicolas.py
@@ -11,6 +11,6 @@ class Nicolas:
 
     @staticmethod
     def raizes(a, b, c):
-        x1 = (-1 * b) + ((-4 * a * c + b**2)**0.5) / (2*a)
-        x2 = (-1 * b) - ((-4 * a * c + b**2)**0.5) / (2*a)
+        x1 = ((-1 * b) + (((-4 * a * c) + (b**2))**0.5)) / (2*a)
+        x2 = ((-1 * b) - (((-4 * a * c) + (b**2))**0.5)) / (2*a)
         return f"O valor das raízes é {x1} e {x2}"
