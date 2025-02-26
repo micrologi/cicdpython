2fc33f3393c728716e489da7b0efbdb77b9ddbe3
EnzoTavares <t.i.aulas.enzo@gmail.com>
2025-02-26 15:37:59 -0300

    ComitEnzoT

--- enzot.py
+++ enzot.py
@@ -1,3 +1,11 @@
-print("Oi professor")
-print("Oi professor2")
-print("Oi professor3")
+class Enzot:
+
+    @staticmethod
+    def mensagem(nome):
+        return f"Olá mundo doido, aqui é o {nome}!"
+
+
+@staticmethod
+def hipotenusa(cateto1, cateto2):
+    valor = (cateto1**2 + cateto2**2)**0.5
+    return f"O valor da hipotenusa é {valor}"
